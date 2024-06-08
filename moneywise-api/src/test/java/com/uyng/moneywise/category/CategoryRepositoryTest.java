14e8e3a7bad8a32cb7d0e2eac29bbae52b6a8a59
Uy Nguyen <uynguyen1501@gmail.com>
2024-06-08 22:08:22 +0200

    add category test

--- moneywise-api/src/test/java/com/uyng/moneywise/category/CategoryRepositoryTest.java
+++ moneywise-api/src/test/java/com/uyng/moneywise/category/CategoryRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -85,5 +87,63 @@ public class CategoryRepositoryTest {
         assertThat(category.getName()).isEqualTo("Groceries");
         assertThat(category.getType()).isEqualTo(CategoryType.EXPENSE);
         assertThat(category.isDefault()).isTrue();
+    }
+
+    @Test
+    public void testFindByIdAndUserEmail() {
+        Optional<Category> foundCategory = categoryRepository.findByIdAndUserEmail(category.getId(), user.getEmail());
+        assertThat(foundCategory).isPresent();
+        assertThat(foundCategory.get().getId()).isEqualTo(category.getId());
+        assertThat(foundCategory.get().getUser()).isEqualTo(user);
+    }
+
+    @Test
+    public void testFindByIdAndUserEmail_InvalidId() {
+        Optional<Category> result = categoryRepository.findByIdAndUserEmail(-1, user.getEmail());
+        assertThat(result).isNotPresent();
+    }
+
+    @Test
+    public void testFindByIdAndUserEmail_InvalidEmail() {
+        Optional<Category> result = categoryRepository.findByIdAndUserEmail(category.getId(), "invalid@example.com");
+        assertThat(result).isNotPresent();
+    }
+
+    @Test
+    public void testFindByUserEmail_ReturnsAllCategories() {
+        Category category2 = categoryRepository.save(
+                Category.builder()
+                        .user(user)
+                        .name("Category2")
+                        .type(CategoryType.INCOME)
+                        .isDefault(false)
+                        .build()
+        );
+        List<Category> expectedCategories = Arrays.asList(category, category2);
+
+        List<Category> actualCategories = categoryRepository.findByUserEmail(user.getEmail());
+
+        assertThat(actualCategories).containsExactlyInAnyOrderElementsOf(expectedCategories);
+    }
+
+    @Test
+    public void testFindByUserEmail_ReturnsCategoriesForDifferentUser() {
+        User user2 = User.builder().email("user2@example.com").build();
+        entityManager.persist(user2);
+
+        Category category2 = categoryRepository.save(
+                Category.builder()
+                        .user(user2)
+                        .name("Category2")
+                        .type(CategoryType.INCOME)
+                        .isDefault(false)
+                        .build()
+        );
+
+        List<Category> actualCategoriesForUser1 = categoryRepository.findByUserEmail(user.getEmail());
+        List<Category> actualCategoriesForUser2 = categoryRepository.findByUserEmail(user2.getEmail());
+
+        assertThat(actualCategoriesForUser1).containsExactly(category);
+        assertThat(actualCategoriesForUser2).containsExactly(category2);
     }
 }
